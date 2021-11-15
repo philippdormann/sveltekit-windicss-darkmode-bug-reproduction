93dcccd51bf4f3c6098eb29ebde4f953f71fc69d
Philipp Dormann <philipp@philippdormann.de>
2021-11-15 10:19:37 +0100

    add more demo content containing the error itself

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,10 @@
 <main class="text-center text-orange-500 dark:text-light-blue-600">
   <h1>Hello world</h1>
   <p class="py-8">this is a demo with SvelteKit and Windi CSS</p>
+  <pre
+    class="bg-gray-300 text-black w-3/4 dark:bg-gray-800 dark:text-white mx-auto p-4 text-left">
+    10:18:10 [vite-plugin-svelte] /src/routes/index.svelte:12:0 Unused CSS selector ".dark .dark\:bg-gray-800"
+10:18:10 [vite-plugin-svelte] /src/routes/index.svelte:34:0 Unused CSS selector ".dark .dark\:text-light-blue-600"
+10:18:10 [vite-plugin-svelte] /src/routes/index.svelte:42:0 Unused CSS selector ".dark .dark\:text-white"
+</pre>
 </main>
